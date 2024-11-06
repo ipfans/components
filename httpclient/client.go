8b765fa17455544f478d1e19b2eabef5223b1038
Kevin Lee <363344+ipfans@users.noreply.github.com>
2024-11-06 09:30:32 +0800

    Update httpclient

--- httpclient/client.go
+++ httpclient/client.go
@@ -1,7 +1,12 @@
 package httpclient
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"net/http"
+	"reflect"
+	"strings"
 	"time"
 
 	"github.com/go-resty/resty/v2"
@@ -12,9 +17,13 @@ import (
 )
 
 type Config struct {
-	Debug     bool          `koanf:"debug"`
-	UserAgent string        `koanf:"user_agent"`
-	Timeout   time.Duration `koanf:"timeout"`
+	UserAgent          string        `koanf:"user_agent"`            // User agent, Default: HttpClient/1.0
+	Timeout            time.Duration `koanf:"timeout"`               // Timeout, Default: 60 seconds
+	RetryCount         int           `koanf:"retry_count"`           // Retry count, Default: 3
+	Debug              bool          `koanf:"debug"`                 // Enable debug log, include request and response. Default: false
+	DisableRetry       bool          `koanf:"disable_retry"`         // Disable retry, Default: false
+	LogAllResponse     bool          `koanf:"log_all_response"`      // Log all response body, Default only json/xml. It only works when debug is true. Default: false
+	MaxRequestBodySize int64         `koanf:"max_request_body_size"` // Max request body size, Default: 5 * 1024 (5KB)
 }
 
 type Option func(*options)
@@ -29,12 +38,24 @@ func defaultOptions() *options {
 	}
 }
 
+// WithLogger sets the logger for the httpclient
 func WithLogger(logger zerolog.Logger) Option {
 	return func(o *options) {
 		o.logger = logger
 	}
 }
 
+func betterPrintHeaders(headers http.Header) string {
+	var builder strings.Builder
+	for k, v := range headers {
+		builder.WriteString(fmt.Sprintf("%s: ", k))
+		builder.WriteString(strings.Join(v, ", "))
+		builder.WriteString("\n")
+	}
+	return builder.String()
+}
+
+// New creates a new resty client
 func New(cfg Config, opts ...Option) *resty.Client {
 	options := defaultOptions()
 	for _, o := range opts {
@@ -56,30 +77,70 @@ func New(cfg Config, opts ...Option) *resty.Client {
 			return nil
 		})
 	if cfg.Debug {
-		client.OnRequestLog(func(req *resty.RequestLog) error {
-			options.logger.Debug().
-				Str("request_id", req.Header.Get("X-Request-ID")).
-				Any("headers", req.Header).
-				Str("body", req.Body).
-				Msg("http request")
+		client = client.OnBeforeRequest(func(client *resty.Client, request *resty.Request) error {
+			logger := options.logger.Debug().Ctx(request.Context()).
+				Str("method", request.Method).
+				Str("url", request.URL).
+				Any("headers", betterPrintHeaders(request.Header))
+			if request.Body != nil {
+				kind := reflect.Indirect(reflect.ValueOf(request.Body)).Type().Kind()
+				switch kind {
+				case reflect.String:
+					logger = logger.Str("body", request.Body.(string))
+				case reflect.Struct, reflect.Map, reflect.Slice:
+					logger = logger.Interface("body", request.Body)
+				default:
+					switch v := request.Body.(type) {
+					case io.Reader:
+						logger = logger.Str("body", "***** REQUEST BODY IS AN IO.READER *****")
+					default:
+						logger = logger.Str("body", fmt.Sprintf("%v", v))
+					}
+				}
+			}
+			logger.Msg("httpclient request")
 			return nil
-		})
-		client.OnResponseLog(func(resp *resty.ResponseLog) error {
-			options.logger.Debug().
-				Str("request_id", resp.Header.Get("X-Request-ID")).
-				Any("headers", resp.Header).
-				Str("body", resp.Body).
-				Msg("http response")
+		}).OnAfterResponse(func(client *resty.Client, response *resty.Response) error {
+			logger := options.logger.Debug().Ctx(response.Request.Context()).
+				Int("status_code", response.StatusCode()).
+				Any("headers", betterPrintHeaders(response.Request.Header))
+			if cfg.LogAllResponse {
+				logger.Str("body", response.String())
+			} else {
+				ct := response.Header().Get("content-type")
+				fileSize := response.Size()
+				if fileSize == 0 {
+					fileSize = response.RawResponse.ContentLength
+				}
+				if fileSize > cfg.MaxRequestBodySize {
+					logger.Str("body", "***** RESPONSE TOO LARGE (size - "+fmt.Sprintf("%d", fileSize)+") *****")
+				} else if resty.IsJSONType(ct) || resty.IsXMLType(ct) {
+					logger.Str("body", response.String())
+				}
+			}
+			logger.Msg("httpclient response")
 			return nil
-		})
-		client.OnError(func(req *resty.Request, err error) {
-			options.logger.Error().
-				Str("request_id", req.Header.Get("X-Request-ID")).
+		}).OnError(func(req *resty.Request, err error) {
+			options.logger.Error().Ctx(req.Context()).
 				Err(err).
 				Msg("http request error")
 		})
 	}
+	if !cfg.DisableRetry {
+		client = client.SetRetryCount(utils.DefaultValue(cfg.RetryCount, 3)).
+			SetRetryMaxWaitTime(10 * time.Second).
+			SetRetryWaitTime(500 * time.Millisecond)
+	}
 	return client
+}
+
+// NewRequest creates a new request with the given context and jwt
+func NewRequest(ctx context.Context, client *resty.Client, jwt string) *resty.Request {
+	req := client.R().SetContext(ctx)
+	if jwt != "" {
+		req = req.SetAuthScheme("Bearer").SetAuthToken(jwt)
+	}
+	return req
 }
 
 type wrapperTransport struct {
@@ -104,6 +165,7 @@ func (t *wrapperTransport) RoundTrip(req *http.Request) (*http.Response, error) 
 	return resp.RawResponse, nil
 }
 
+// WrapClient wraps the resty client to add request id to the request
 func WrapClient(client *resty.Client) *http.Client {
 	return &http.Client{
 		Transport: &wrapperTransport{client: client},
